46f2b0c51b636dc5dcf1c505d21af9a21f90cb7a
itay-cyber <57634628+itay-cyber@users.noreply.github.com>
2019-11-18 16:21:35 +0000

    Create addons.js

---
+++ addons.js
@@ -0,0 +1,28 @@
+ var B = document.getElementById("body");
+    var H = document.getElementById("mainH");
+ 
+   
+    H.style.textAlign="center";
+
+
+  
+    function Get_info(){
+        var input = document.getElementById("usr");
+        var answer = document.getElementById("output");
+     var A = document.getElementById("img");
+        if (input.value=="help"){
+            answer.innerHTML="search something we may have info";
+        };
+       if (input.value=="sebastian"|| input.value=="Sebastian"){
+            answer.innerHTML="nationality: indonesien, Hair Color: black";
+            A.hidden = "hidden";
+        };
+     if (input.value=="itay" || input.value=="Itay"){
+            answer.innerHTML="nationality: Hebrew, Hair Color: braces";
+      A.hidden="";
+            A.src = "https://raw.githubusercontent.com/ccm7676/data-base/master/Photo%20on%2014-11-2019%20at%2013.32.jpg";
+  
+        };
+        
+    };
+    
